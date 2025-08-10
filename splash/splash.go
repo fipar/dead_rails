a63313b336fd5a54eb3b4c4cb8100d8a8c520f68
Fernando Ipar <fipar@acm.org>
2025-08-10 00:01:23 -0300

    Partially working splash screen

---
+++ splash/splash.go
@@ -0,0 +1,40 @@
+components {
+  id: "sprite"
+  component: "/splash/splash.sprite"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  scale {
+    x: 1.0
+    y: 1.0
+    z: 1.0
+  }
+}
+components {
+  id: "script"
+  component: "/splash/splash.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  scale {
+    x: 1.0
+    y: 1.0
+    z: 1.0
+  }
+}
